91698471477808e3ac5ca73b05cf484056ae349e
Jrespect <whsxoddl5775@naver.com>
2020-07-22 09:26:31 +0900

    200722_Jrespect

--- mbti/urls.py
+++ mbti/urls.py
@@ -4,9 +4,8 @@ from . import views
 urlpatterns = [   
     path('',views.intro),
     path('signin/',views.signin),
-    path('signin/q1/',views.Crawling_Image),
+    path('signin/q1/',views.q1),
     path('result/',views.result),
     path('info_inquiry/', views.info_inquiry),
     path('show_result/',views.showResult, name='show_result')
-    
 ]
