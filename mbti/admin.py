34f74bed7be110b4c31b546c7a689929f35645dd
jsr0220 <jsr0220@naver.com>
2020-07-22 16:53:37 +0900

    0722 453

--- mbti/admin.py
+++ mbti/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import MbtiResult
+from .models import inputClient, MbtiResult
 
 # Register your models here.
 admin.site.register(MbtiResult)
+# admin.site.register(lawywerAccount)
+admin.site.register(inputClient)
